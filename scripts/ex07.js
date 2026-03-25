227d081e00e513cf437c5a25b0a66eac394d4c9c
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-03-25 12:51:37 -0300

    add - 07 commit incompleto

---
+++ scripts/ex07.js
@@ -0,0 +1,15 @@
+function sistema(){
+    let sabor = prompt("escolha o seu saboour de a até i");
+
+    switch(sabor){
+        case "a":
+            alert("picole de choocolate e 1,50")
+
+
+
+    }
+
+
+
+
+}
