7fd4521a789023408bba76944ef9c273f6450300
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-03-30 11:19:25 -0300

    add - ex08

---
+++ scripts/ex08.js
@@ -0,0 +1,14 @@
+function variaveldual(){
+    let a = parseInt(prompt("digite seu primeiro numero"))
+    let b = parseInt(prompt("digite seu segundo numero"))
+
+    let dif = (a - b);
+    let dobro = (a + (b*3));
+    let mult = (a * b);
+
+    alert("a diferença entre os dois é de : " + dif);
+    alert("O dobro da primeira variável somado ao triplo da segunda é : " + dobro);
+    alert("a multiplicação das duas variaveis é : " + mult);
+
+
+}
