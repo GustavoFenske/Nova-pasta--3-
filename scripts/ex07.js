66d09936319c1a72ba76338cc940ccbda6d499aa
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-03-30 10:55:49 -0300

    add - ex07

--- scripts/ex07.js
+++ scripts/ex07.js
@@ -1,9 +1,35 @@
-function sistema(){
-    let sabor = prompt("escolha o seu saboour de a até i");
+function sistema() {
+    let sabor = prompt("escolha o seu saboour picole : \nChocolate [a]\n" + "\n Morango [b]\n" + "\n Creme [c]\n" + "\n Manga [d]\n" + "\n Melancia [e]\n" + "\n vanilla ice [f]\n" + "\n Ceu azul [g]\n" + "\n Brownie [h]\n" + "\n Hawaiano [i]\n");
 
-    switch(sabor){
+
+    switch (sabor) {
         case "a":
             alert("picole de choocolate e 1,50")
+            break;
+        case "b":
+            alert("picole de Morango e 2,50")
+            break;
+        case "c":
+            alert("picole de Creme e 2,50")
+             break;
+        case "d":
+            alert("picole de Manga e 3,20")
+             break;
+        case "e":
+            alert("picole de Melancia e 3,40")
+             break;
+        case "f":
+            alert("picole de vanilla ice e 3,00")
+             break;
+        case "g":
+            alert("picole de Ceu azul e 3,60")
+            break;
+        case "h":
+            alert("picole de Brownie e 4,00")
+             break;
+        case "i":
+            alert("picole de Hawaiano e 5,00")
+             break;
 
 
 
